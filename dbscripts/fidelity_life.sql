0abae841554021410b8afcc387e796e1e92f835c
payal <payal.jindal@complereinfosystem.com>
2021-10-21 12:04:13 +0530

    SQL QUERY ADDITION

---
+++ dbscripts/fidelity_life.sql
@@ -0,0 +1,6 @@
+create schema dev;
+create table fidelity_test_users (
+  name varchar (100),  -- variable string column
+  preferences string, -- column used to store JSON type of data
+  created_at timestamp
+);
