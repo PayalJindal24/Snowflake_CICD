f97ec8ba8f4f344efab3087f8175db8a51688fae
PayalJindal24 <75351063+PayalJindal24@users.noreply.github.com>
2021-10-22 13:28:17 +0530

    Update v1.1.1__fidelity_life.sql
    Added a print cmd in db scripts

--- dbscripts/v1.1.1__fidelity_life.sql
+++ dbscripts/v1.1.1__fidelity_life.sql
@@ -1,5 +1,6 @@
  use database demo_db;
 use schema schemachange;
+Select 'abc';
 create table fidelity_test_users (
   name varchar (100),  -- variable string column
   preferences string, -- column used to store JSON type of data
