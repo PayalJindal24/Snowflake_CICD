65b805fabcc29ae1deae64a668750084b0202df8
payal <payal.jindal@complereinfosystem.com>
2021-10-22 16:17:24 +0530

    v7 script update -1

--- dbscripts/V1.1.7__fidelity_life.sql
+++ dbscripts/V1.1.7__fidelity_life.sql
@@ -2,4 +2,4 @@ use warehouse COMPUTE_WH;
 
 use database SNOWFLAKE_GITHUB;
 use SCHEMA DEV;
-insert into table hello_world_test values('payal','jindal',1);
+insert into  hello_world_test values('payal','jindal',1);
